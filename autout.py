d2364fa819f2e984f1beaf5092fd7f132ceb7316
r0fls <raphael.deem@gmail.com>
2015-12-15 20:34:09 -0800

    skeleton, needs arg type

--- autout.py
+++ autout.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
         tree = parse_ast(filename)
         filename = filename.replace('.py','')
         f = open('test_{}.py'.format(filename),'w')
-        f.write('import unittest\n\nclass Test{}(unittest.TestCase):\n\n'.format(filename))
+        f.write('import unittest\nimport {0}\n\nclass Test_{0}(unittest.TestCase):\n\n'.format(filename))
         for func in top_level_functions(tree.body):
-            f.write("    def test_{}(self):\n    self.assertEqual\n\n".format(func.name))
+            f.write("    def test_{1}(self):\n        self.assertEqual({0}.{1}(),)\n\n".format(filename,func.name))
             print("  %s" % func.name)
         f.write("if __name__ == '__main__':\n    unittest.main()")
